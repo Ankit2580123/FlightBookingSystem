445bfc21dd1b975db93dde34469f805df98203b6
Ankit2580123 <ankitkumar2580123@gmail.com>
2026-03-29 17:05:20 +0530

    Removed .vs and added gitignore

--- BookingService/obj/Release/net8.0/BookingService.AssemblyInfo.cs
+++ BookingService/obj/Release/net8.0/BookingService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a62c33e0e8eef3f2c1136d9da8681226d02495")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
